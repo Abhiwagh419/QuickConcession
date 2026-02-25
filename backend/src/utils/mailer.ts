bed0154a0f8a0a18680c75e008268786ce685489
Asus <waghshailesh151@gmail.com>
2026-02-25 20:30:02 +0530

    Switch to SenderGrid

--- backend/src/utils/mailer.ts
+++ backend/src/utils/mailer.ts
@@ -2,12 +2,12 @@ import * as nodemailer from "nodemailer";
 import { sendMail } from "./sendMail";
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
+  host: "smtp.sendgrid.net",
+  port: 587,
   secure: false,
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
+    user: "apikey",
+    pass: process.env.SENDGRID_API_KEY,
   },
 });
 
